a6204855d5c2aba0e383264549fa80340f4d2fca
Calvert <wongcalvert-work@outlook.com>
2025-06-16 22:32:14 -0700

    Added missing Image props in test

--- __tests__/components/atoms/Image.test.tsx
+++ __tests__/components/atoms/Image.test.tsx
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom'
 
 describe("Image component test", async () => {
 	test("renders with src and alt", async () => {
-		render(<Image src="example.jpg" alt="Example Image" />);
+		render(<Image height={50} width={50} src="example.jpg" alt="Example Image" />);
 
 		const img = screen.getByRole("img");
 		expect(img).toBeInTheDocument();
